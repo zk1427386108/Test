b88a810e98d57d59fec9dee622c083925e9362ff
zk <zk1427386108@163.com>
2019-01-04 11:44:45 +0800

    111

--- src/main/java/com/example/demo/biz/TestBiz.java
+++ src/main/java/com/example/demo/biz/TestBiz.java
@@ -3,5 +3,7 @@ package com.example.demo.biz;
 public class TestBiz {
 	public static void main(String[] args) {
 		System.out.println("hehe");
+		System.out.println("hehe");
+		System.out.println("hehe");
 	}
 }
