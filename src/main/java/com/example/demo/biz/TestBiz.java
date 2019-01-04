0ea09c74703c331efeafccf0e860b8f97a1e12ea
zk <zk1427386108@163.com>
2019-01-04 11:45:37 +0800

    keyi

--- src/main/java/com/example/demo/biz/TestBiz.java
+++ src/main/java/com/example/demo/biz/TestBiz.java
@@ -2,8 +2,8 @@ package com.example.demo.biz;
 
 public class TestBiz {
 	public static void main(String[] args) {
-		System.out.println("hehe");
-		System.out.println("hehe");
+		System.out.println("he2he");
+		System.out.println("he1he");
 		System.out.println("hehe");
 	}
 }
